447249258033b9292bda5a439ab3b9f951d56f98
hubertsulzycki <87582624+hubertsulzycki@users.noreply.github.com>
2025-03-12 20:13:03 +0100

    Add Lab 1 and Lab 2

---
+++ ServletsJSPJSTL/src/main/java/lab/servletsjspjstl/ColorBean.java
@@ -0,0 +1,31 @@
+package lab.servletsjspjstl;
+
+public class ColorBean {
+    private String foregroundColor;
+    private String backgroundColor;
+    private String borderType;
+
+    public String getForegroundColor() {
+        return foregroundColor;
+    }
+
+    public void setForegroundColor(String foregroundColor) {
+        this.foregroundColor = foregroundColor;
+    }
+
+    public String getBackgroundColor() {
+        return backgroundColor;
+    }
+
+    public void setBackgroundColor(String backgroundColor) {
+        this.backgroundColor = backgroundColor;
+    }
+
+    public String getBorderType() {
+        return borderType;
+    }
+
+    public void setBorderType(String borderType) {
+        this.borderType = borderType;
+    }
+}
