e924326cc378ccb154581e5896e021dc01b088f3
hubertsulzycki <87582624+hubertsulzycki@users.noreply.github.com>
2025-03-13 21:25:13 +0100

    Add Lab 3

---
+++ Requests/src/main/java/lab/requests/backing/RequestsList.java
@@ -0,0 +1,61 @@
+package lab.requests.backing;
+
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.faces.component.html.HtmlDataTable;
+import jakarta.inject.Inject;
+import jakarta.inject.Named;
+import jakarta.transaction.Transactional;
+import jakarta.validation.constraints.Size;
+import lab.requests.data.RequestRepository;
+import lab.requests.entities.Request;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@RequestScoped
+@Named
+public class RequestsList {
+    @Inject
+    private RequestRepository requestRepository;
+
+    @Size(min = 3, max = 60, message = "{request.size}")
+    private String newRequest;
+    private HtmlDataTable requestsDataTable;
+
+    public List<Request> getAllRequests() {
+        return requestRepository.findAll();
+    }
+
+    public String getNewRequest() {
+        return newRequest;
+    }
+
+    public void setNewRequest(String newRequest) {
+        this.newRequest = newRequest;
+    }
+
+    public HtmlDataTable getRequestsDataTable() {
+        return requestsDataTable;
+    }
+
+    public void setRequestsDataTable(HtmlDataTable requestsDataTable) {
+        this.requestsDataTable = requestsDataTable;
+    }
+
+    @Transactional
+    public String addRequest()
+    {
+        Request request = new Request();
+        request.setRequestText(newRequest);
+        request.setRequestDate(LocalDate.now());
+        requestRepository.create(request);
+        return "requestsList.xhtml?faces-redirect=true";
+    }
+    @Transactional
+    public String deleteRequest() {
+        Request request = (Request) getRequestsDataTable().getRowData();
+        requestRepository.remove(request);
+        return "requestsList.xhtml?faces-redirect=true";
+    }
+
+}
