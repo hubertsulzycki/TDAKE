ce3db48eff28e57d54bfd91247827f73ad03fab8
Entharen <sulzycki.hub@gmail.com>
2025-04-23 22:53:03 +0200

    Add: Lab 7

---
+++ WebApiAjax/WebApiAjax/Program.cs
@@ -0,0 +1,31 @@
+using WebApiAjax;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/math",
+ (int x, int y) => new CalcResult()
+ {
+     Sum = x + y,
+     Difference = x - y,
+     Product = x * y,
+     Quotient = x / y
+ });
+
+app.UseStaticFiles();
+app.Run();
+
+internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
